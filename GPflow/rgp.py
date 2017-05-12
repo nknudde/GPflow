0c909238c378b76abea801f76671c307e49ac5c9
nknudde <nicolas.knudde@ugent.be>
2017-05-12 14:56:39 +0200

    Add prediction formulas

--- GPflow/rgp.py
+++ GPflow/rgp.py
@@ -22,6 +22,7 @@ class RGP(Model):
         assert len(kernels) >= 2
         N = np.shape(Y)[0]
         H = len(kernels)
+        self.N = N
         self.Lt = Lt
 
         layers.append(InputLayer(kernels[0], N, Qs[0], Lt, Ms[0]))
@@ -32,9 +33,35 @@ class RGP(Model):
 
     def build_likelihood(self):
         b, Xm, Xv = self.layers[0].build_likelihood(self.Lt)
-        for i in range(1, len(self.layers)-1):
+        for i in range(1, len(self.layers) - 1):
             b, Xm, Xv = self.layers[i].build_likelihood(self.Lt, Xm, Xv)
         return self.layers[-1].build_likelihood(self.Lt, Xm, Xv)
+
+    @AutoFlow((tf.int32, []))
+    def predict_f(self, num_samples):
+        X_m = []
+        X_v = []
+        xm, xv, xmm, xvm = self.layers[0].predict_x(self.N, self.Lt, init=True)
+        X_m.append(xm)
+        X_v.append(xv)
+        for i in range(1, len(self.layers) - 1):
+            xm, xv, xmm, xvm = self.layers[i].predict_x(self.N, self.Lt, xm, xv, xmm, xvm, init=True)
+            X_m.append(xm)
+            X_v.append(xv)
+        Ym, Yv = self.layers[-1].predict_x(self.N, self.Lt, xm, xv, xmm, xvm)
+        for i in range(num_samples - 1):
+            xm, xv, xmm, xvm = self.layers[0].predict_x(self.N, self.Lt, X_cms=X_m[0], X_cvs=X_v[0])
+            X_m[0] = xm
+            X_v[0] = xv
+            for i in range(1, len(self.layers) - 1):
+                xm, xv, xmm, xvm = self.layers[i].predict_x(self.N, self.Lt, xm, xv, xmm, xvm, X_cms=X_m[i],
+                                                            X_cvs=X_v[i])
+                X_m.append(xm)
+                X_v.append(xv)
+            ym, yv = self.layers[-1].predict_x(self.N, self.Lt, xm, xv, xmm, xvm)
+            Ym = tf.concat([Ym, ym], 0)
+            Yv = tf.concat([Yv, yv], 0)
+        return Ym, Yv
 
 
 class Layer(Parameterized):
@@ -103,6 +130,53 @@ class HiddenLayer(Layer):
         bound += ent2
         return bound, self.X_mean, self.X_var
 
+    def predict_x(self, N, Lt, X_pms, X_pvs, Xp_m, Xp_v, init=False, X_cms=None, X_cvs=None):
+        if init:
+            X_cms = tf.slice(self.X_mean, [N - Lt, 0], [-1, -1])
+            X_cvs = tf.slice(self.X_var, [N - Lt, 0], [-1, -1])
+        X_m = tf.concat([self.hankel(tf.slice(self.X_mean, [0, 0], [N - 1, -1]), Lt),
+                         self.hankel(tf.slice(Xp_m, [1, 0], [N - 1, -1]), Lt)], 1)
+        X_v = tf.concat([self.hankel(tf.slice(self.X_var, [0, 0], [N - 1, -1]), Lt),
+                         self.hankel(tf.slice(Xp_v, [1, 0], [N - 1, -1]), Lt)], 1)
+
+        Xrms = tf.concat([X_cms, X_pms], 1)
+        Xrvs = tf.concat([X_cvs, X_pvs], 1)
+        num_inducing = tf.shape(self.Z)[0]
+        psi1 = self.kern.eKxz(self.Z, X_m, X_v)
+        psi2 = tf.reduce_sum(self.kern.eKzxKxz(self.Z, X_m, X_v), 0)
+        psi0star = tf.reduce_sum(self.kern.eKdiag(Xrms, Xrvs), 0)  # N*
+        psi1star = self.kern.eKxz(self.Z, Xrms, Xrvs)  # N* x M
+        psi2star = tf.reduce_sum(self.kern.eKzxKxz(self.Z, Xrms, Xrvs), 0)
+        X_mo = tf.slice(self.X_m, [Lt, 0], [-1, -1])
+
+        Kuu = self.kern.K(self.Z) + tf.eye(num_inducing, dtype=float_type) * 1e-6  # M x M
+        sigma2 = self.likelihood.variance
+        sigma = tf.sqrt(sigma2)
+        L = tf.cholesky(Kuu)  # M x M
+
+        A = tf.matrix_triangular_solve(L, tf.transpose(psi1), lower=True) / sigma  # M x N
+        tmp = tf.matrix_triangular_solve(L, psi2, lower=True)  # M x M
+        AAT = tf.matrix_triangular_solve(L, tf.transpose(tmp), lower=True) / sigma2
+        B = AAT + tf.eye(num_inducing, dtype=float_type)
+        LB = tf.cholesky(B)  # M x M
+        c = tf.matrix_triangular_solve(LB, tf.matmul(A, X_mo), lower=True) / sigma  # M x p
+        tmp1 = tf.matrix_triangular_solve(L, tf.transpose(psi1star), lower=True)
+        tmp2 = tf.matrix_triangular_solve(LB, tmp1, lower=True)
+        mean = tf.matmul(tmp2, c, transpose_a=True)
+
+        # All of these: M x M
+        tmp4 = tf.matmul(tmp1, tmp1, transpose_b=True)
+        tmp5 = tf.matrix_triangular_solve(L, tf.transpose(tf.matrix_triangular_solve(L, psi2star)))
+        tmp6 = tf.matrix_triangular_solve(LB, tf.transpose(tf.matrix_triangular_solve(LB, tmp5)))
+
+        TT = tf.reduce_sum(tf.trace(tmp5 - tmp6))  # 1
+        diagonals = psi0star - TT  # 1
+        covar1 = tf.diag_part(tf.matmul(c, tf.matmul(tmp6 - tmp4, c), transpose_a=True))  # p x p
+        covar = tf.expand_dims(covar1 + diagonals, 1)
+        X_cms = tf.slice(tf.concat([X_m, mean], 0), [1, 0], [-1, -1])
+        X_cvs = tf.slice(tf.concat([X_m, covar], 0), [1, 0], [-1, -1])
+        return X_cms, X_cvs, self.X_mean, self.X_var
+
 
 class InputLayer(Layer):
     def __init__(self, kernel, N, Q, Lt, M):
@@ -149,6 +223,49 @@ class InputLayer(Layer):
         bound += ent2
         return bound, self.X_mean, self.X_var
 
+    def predict_x(self, N, Lt, init=False, X_cms=None, X_cvs=None):
+        if init:
+            X_cms = tf.slice(self.X_mean, [N - Lt, 0], [-1, -1])
+            X_cvs = tf.slice(self.X_var, [N - Lt, 0], [-1, -1])
+        X_m = self.hankel(tf.slice(self.X_mean, [0, 0], [N - 1, -1]), Lt)
+        X_v = self.hankel(tf.slice(self.X_var, [0, 0], [N - 1, -1]), Lt)
+
+        num_inducing = tf.shape(self.Z)[0]
+        psi1 = self.kern.eKxz(self.Z, X_m, X_v)
+        psi2 = tf.reduce_sum(self.kern.eKzxKxz(self.Z, X_m, X_v), 0)
+        psi0star = tf.reduce_sum(self.kern.eKdiag(X_cms, X_cvs), 0)  # N*
+        psi1star = self.kern.eKxz(self.Z, X_cms, X_cvs)  # N* x M
+        psi2star = tf.reduce_sum(self.kern.eKzxKxz(self.Z, X_cms, X_cvs), 0)
+        X_mo = tf.slice(self.X_m, [Lt, 0], [-1, -1])
+
+        Kuu = self.kern.K(self.Z) + tf.eye(num_inducing, dtype=float_type) * 1e-6  # M x M
+        sigma2 = self.likelihood.variance
+        sigma = tf.sqrt(sigma2)
+        L = tf.cholesky(Kuu)  # M x M
+
+        A = tf.matrix_triangular_solve(L, tf.transpose(psi1), lower=True) / sigma  # M x N
+        tmp = tf.matrix_triangular_solve(L, psi2, lower=True)  # M x M
+        AAT = tf.matrix_triangular_solve(L, tf.transpose(tmp), lower=True) / sigma2
+        B = AAT + tf.eye(num_inducing, dtype=float_type)
+        LB = tf.cholesky(B)  # M x M
+        c = tf.matrix_triangular_solve(LB, tf.matmul(A, X_mo), lower=True) / sigma  # M x p
+        tmp1 = tf.matrix_triangular_solve(L, tf.transpose(psi1star), lower=True)
+        tmp2 = tf.matrix_triangular_solve(LB, tmp1, lower=True)
+        mean = tf.matmul(tmp2, c, transpose_a=True)
+
+        # All of these: M x M
+        tmp4 = tf.matmul(tmp1, tmp1, transpose_b=True)
+        tmp5 = tf.matrix_triangular_solve(L, tf.transpose(tf.matrix_triangular_solve(L, psi2star)))
+        tmp6 = tf.matrix_triangular_solve(LB, tf.transpose(tf.matrix_triangular_solve(LB, tmp5)))
+
+        TT = tf.reduce_sum(tf.trace(tmp5 - tmp6))  # 1
+        diagonals = psi0star - TT  # 1
+        covar1 = tf.diag_part(tf.matmul(c, tf.matmul(tmp6 - tmp4, c), transpose_a=True))  # p x p
+        covar = tf.expand_dims(covar1 + diagonals, 1)
+        X_cms = tf.slice(tf.concat([X_m, mean], 0), [1, 0], [-1, -1])
+        X_cvs = tf.slice(tf.concat([X_m, covar], 0), [1, 0], [-1, -1])
+        return X_cms, X_cvs
+
 
 class OutputLayer(Layer):
     def __init__(self, kernel, N, Q, Lt, M, Y):
@@ -186,3 +303,43 @@ class OutputLayer(Layer):
         bound += -0.5 * D * log_det_B
         bound += 0.5 * tf.reduce_sum(tf.square(c))
         return bound
+
+    def predict_x(self, N, Lt, X_pms, X_pvs, Xp_m, Xp_v):
+        X_m = self.hankel(tf.slice(Xp_m, [1, 0], [N - 1, -1]), Lt)
+        X_v = self.hankel(tf.slice(Xp_v, [1, 0], [N - 1, -1]), Lt)
+
+        Xrms = X_pms
+        Xrvs = X_pvs
+        num_inducing = tf.shape(self.Z)[0]
+        psi1 = self.kern.eKxz(self.Z, X_m, X_v)
+        psi2 = tf.reduce_sum(self.kern.eKzxKxz(self.Z, X_m, X_v), 0)
+        psi0star = tf.reduce_sum(self.kern.eKdiag(Xrms, Xrvs), 0)  # N*
+        psi1star = self.kern.eKxz(self.Z, Xrms, Xrvs)  # N* x M
+        psi2star = tf.reduce_sum(self.kern.eKzxKxz(self.Z, Xrms, Xrvs), 0)
+        X_mo = tf.slice(self.X_m, [Lt, 0], [-1, -1])
+
+        Kuu = self.kern.K(self.Z) + tf.eye(num_inducing, dtype=float_type) * 1e-6  # M x M
+        sigma2 = self.likelihood.variance
+        sigma = tf.sqrt(sigma2)
+        L = tf.cholesky(Kuu)  # M x M
+
+        A = tf.matrix_triangular_solve(L, tf.transpose(psi1), lower=True) / sigma  # M x N
+        tmp = tf.matrix_triangular_solve(L, psi2, lower=True)  # M x M
+        AAT = tf.matrix_triangular_solve(L, tf.transpose(tmp), lower=True) / sigma2
+        B = AAT + tf.eye(num_inducing, dtype=float_type)
+        LB = tf.cholesky(B)  # M x M
+        c = tf.matrix_triangular_solve(LB, tf.matmul(A, self.Y), lower=True) / sigma  # M x p
+        tmp1 = tf.matrix_triangular_solve(L, tf.transpose(psi1star), lower=True)
+        tmp2 = tf.matrix_triangular_solve(LB, tmp1, lower=True)
+        mean = tf.matmul(tmp2, c, transpose_a=True)
+
+        # All of these: M x M
+        tmp4 = tf.matmul(tmp1, tmp1, transpose_b=True)
+        tmp5 = tf.matrix_triangular_solve(L, tf.transpose(tf.matrix_triangular_solve(L, psi2star)))
+        tmp6 = tf.matrix_triangular_solve(LB, tf.transpose(tf.matrix_triangular_solve(LB, tmp5)))
+
+        TT = tf.reduce_sum(tf.trace(tmp5 - tmp6))  # 1
+        diagonals = psi0star - TT  # 1
+        covar1 = tf.diag_part(tf.matmul(c, tf.matmul(tmp6 - tmp4, c), transpose_a=True))  # p x p
+        covar = tf.expand_dims(covar1 + diagonals, 1)
+        return mean, covar
